803989bfef3f8c502a550d4fbe8198f656e62539
Clara <44407872+Clarakohn12@users.noreply.github.com>
2021-04-06 22:50:59 -0400

    Add files via upload

--- BookStore/Frontend/src/app/components/register/register.component.ts
+++ BookStore/Frontend/src/app/components/register/register.component.ts
@@ -1,5 +1,11 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../user';
+import { UserService } from '../../user.service';
 
+/**
+ * This component allows a user to register for an account.
+ */
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,11 +13,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  //Roles: any = ['Admin', 'Author', 'Reader'];
-
-  constructor() { }
+  // The user service allows for interaction with the backend.
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * This creates an account.
+   * 
+   * @param values is three strings, unless one of the fields wasn't filled out.
+   */
+  createAccount(values: any) {
+    
+    let { username, email, password } = values;
+
+    // Check that all fields are filled out.
+    if (username == "" || email == "" || password == "")
+      return alert("Please fill out all text fields.");
+
+    // Create a new user with the values and save the user to the database.
+    var user = new User(username, email, password)
+      
+    this.userService.addUser(user).subscribe(
+      (response: User) => {
+        alert("Registration successful. You may now login.");
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    );
   }
 
 }
