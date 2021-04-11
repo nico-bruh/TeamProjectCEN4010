88edd4e40bea94089f06e033c76527103d0b12b3
Clara <44407872+Clarakohn12@users.noreply.github.com>
2021-04-11 15:52:04 -0400

    Add files via upload

---
+++ src/main/java/com/testapplication/bookdetails/model/ShoppingCart.java
@@ -0,0 +1,88 @@
+package com.testapplication.bookdetails.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class ShoppingCart implements Serializable {
+
+    @Id
+    private Integer id;
+    private Integer userId;
+    private String bookName;
+    private Integer quantityInCart;
+    private boolean saveForLater;
+    private Integer bookPrice;
+
+    public ShoppingCart(Integer isbn, int id, String bookName, Integer quantityInCart, boolean saveForLater, Integer bookPrice) {
+        this.id = isbn;
+        this.userId = id;
+        this.bookName = bookName;
+        this.quantityInCart = quantityInCart;
+        this.bookPrice = bookPrice;
+        this.saveForLater = saveForLater;
+    }
+
+    public ShoppingCart() {
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public Integer getQuantityInCart() {
+        return quantityInCart;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isSaveForLater() {
+        return saveForLater;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSaveForLater(boolean saveForLater) {
+        this.saveForLater = saveForLater;
+    }
+
+    public void setQuantityInCart(Integer quantityInCart) {
+        this.quantityInCart = quantityInCart;
+    }
+
+    public Integer getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(Integer bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", bookName='" + bookName + '\'' +
+                ", quantityInCart=" + quantityInCart +
+                ", saveForLater=" + saveForLater +
+                ", bookPrice=" + bookPrice +
+                '}';
+    }
+}
