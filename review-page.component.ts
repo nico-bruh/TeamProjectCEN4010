539e4d5ecbd236b0b460c1c82ad153f45c49a5c6
mkrau009 <70150846+mkrau009@users.noreply.github.com>
2021-04-14 22:17:42 -0400

    Add files via upload

--- review-page.component.ts
+++ review-page.component.ts
@@ -1,7 +1,9 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { error } from 'selenium-webdriver';
 import { Book } from '../book';
 import { BookService } from '../book.service';
 
@@ -14,6 +16,7 @@ export class ReviewPageComponent implements OnInit {
 
   private routeSub: Subscription;
   public book: Book;
+  public reviewBook: Book;
 
   constructor(private bookService: BookService, private route: ActivatedRoute) { }
 
@@ -28,5 +31,50 @@ public getBook(id:string): void {
     ,(error: HttpErrorResponse) => {alert(error.message);}
     );
   }
+
+  public onSubmit(book: NgForm): void{
+
+    this.bookService.updateBook(book.value).subscribe(
+      (response: Book)=>{
+        alert("Review submitted!");
+      },
+      (error: HttpErrorResponse)=>{
+        alert(error.message);
+      },
+    )
+  }
+
+
+  // public onSubmit(values: any){
+
+  //   let {title, rating, review} = values;
+  //   alert(rating);
+  //   alert(review);
+  //   this.bookService.getABook(title).subscribe(
+
+  //     (response: Book)=>{
+  //       if(response != null)
+  //         this.book = response;
+  //     },// If there is an error, print the error message.
+  //     (error: HttpErrorResponse) => {
+  //       alert(error.message)
+  //     }
+
+  //   )
+  //   this.book.rating = rating;
+  //   this.book.review = review;
+  //   this.bookService.updateBook(this.book).subscribe(
+
+  //     (response: Book)=>{
+  //       if(response == null)
+  //         alert("Book does not exist.")
+  //     },// If there is an error, print the error message.
+  //     (error: HttpErrorResponse) => {
+  //       alert(error.message)
+  //     }
+
+  //   )
+
+  // }
 
 }
