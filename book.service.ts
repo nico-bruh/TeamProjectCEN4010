2fd8c611bcb756db103fe4a4803a3b8d67548f78
mkrau009 <70150846+mkrau009@users.noreply.github.com>
2021-04-14 22:16:58 -0400

    Add files via upload

--- book.service.ts
+++ book.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
@@ -18,6 +19,9 @@ export class BookService {
 
   public getBook(id: String): Observable<any>{
     return this.http.get<any>(`${this.apiServerUrl}/book/all`);
+  }
+  public getABook(title: String): Observable<any>{
+    return this.http.get<any>(`${this.apiServerUrl}/book/getByTitle`);
   }
 
   public addBook(book: Book): Observable<Book>{
--- book.ts
+++ book.ts
@@ -2,16 +2,16 @@ import { logging } from "selenium-webdriver";
 
 export interface Book {
     
-    title: String;
-    author: String;
-    imageURL: String;
-    description: String;
-    price: number;
-    id: String;
-    genre: String;
-    release_date: String;
-    publishingInfo: String;
-    rating: number;
-    review: String;
+    title?: String;
+    author?: String;
+    imageURL?: String;
+    description?: String;
+    price?: number;
+    id?: String;
+    genre?: String;
+    release_date?: String;
+    publishingInfo?: String;
+    rating?: number;
+    review?: String;
 
 }
